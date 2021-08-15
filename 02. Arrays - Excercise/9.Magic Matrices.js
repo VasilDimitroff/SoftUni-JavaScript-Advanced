bd48455fc65328c423a2fb5c02dbb906e497eb28
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-08-15 18:49:27 +0300

    task solution added

---
+++ 02. Arrays - Excercise/9.Magic Matrices.js
@@ -0,0 +1,37 @@
+function magicMatrices(matrix){
+
+        matrix.map(row => row.map(col => Number(col)));
+
+        if (matrix.length < 1) {
+            matrix.length = 5;
+            matrix.fill(0);
+        }
+
+        let sum = function(arr) {
+            return arr.reduce(function(a, b){ return a + b; }, 0);
+          };
+      
+         let verticalSumsArray =  matrix.map(function(row, i) {
+            return sum(matrix.map(function(row) { return row[i]; }));
+          }); 
+        
+          let areEqual = true;
+
+          for (let row = 0; row < matrix.length; row++) {
+            let currentSum = 0;
+            for (let col = 0; col < matrix.length; col++) {
+               
+                currentSum += matrix[row][col];
+            }    
+            
+            if (currentSum != verticalSumsArray[row]) {
+                areEqual = false;
+            }
+          }
+     
+        console.log(areEqual);
+}
+
+magicMatrices([[1, 0, 0],
+    [0, 0, 1],
+    [0, 1, 0]]);
