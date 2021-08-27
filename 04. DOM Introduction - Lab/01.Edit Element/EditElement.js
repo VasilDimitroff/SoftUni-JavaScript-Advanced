23fdca47c288cd8dd76f6d22424ad78f5b209dd3
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-08-27 13:14:30 +0300

    Update EditElement.js

--- 04. DOM Introduction - Lab/01.Edit Element/EditElement.js
+++ 04. DOM Introduction - Lab/01.Edit Element/EditElement.js
@@ -1,10 +1,14 @@
 function editElement(elementById, match, replacer) {
+    // First parameter is a reference to an HTML element, other two parameters are string –
+    // match and replacer.
+   // You have to replace all occurrences of match inside the text content of the given elements
+    // with replacer.
     console.log(elementById);
 
     let element = elementById;
 
     if (element.innerHTML.includes(match)) {
         let newStr = element.innerHTML.replace(match, replacer);
-        element.innerHTML = newSt;
+        element.innerHTML = newStr;
     }     
 }
