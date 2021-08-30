18ddd9845badf7e60d4b4e1288c5663956e63b87
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-08-30 11:40:16 +0300

    Colorize Table problem solved

--- 04. DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
+++ 04. DOM Introduction - Lab/05.Colorize Table/ColorizeTable.js
@@ -1,3 +1,12 @@
 function colorize() {
-    // TODO
+    let rows = document.getElementsByTagName('tr');
+    
+    for (let index = 0; index < rows.length; index++) {
+        const element = rows[index];
+
+        if (index % 2 != 0) {
+            element.style.backgroundColor = 'Teal';
+        }
+        
+    }
 }
