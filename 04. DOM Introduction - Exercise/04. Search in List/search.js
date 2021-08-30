f9187732d812d187772c71d9318e141fe83da38f
Vasil Dimitrov <v.b.dimitrow@gmai.com>
2021-08-30 22:10:51 +0300

    Search in List problem solved

--- 04. DOM Introduction - Exercise/04. Search in List/search.js
+++ 04. DOM Introduction - Exercise/04. Search in List/search.js
@@ -1,3 +1,28 @@
 function search() {
-   // TODO
+
+   let keyword = document.querySelector('#searchText').value;
+
+   let targetTowns = [];
+
+   let allTowns = document.getElementsByTagName('li');
+
+   for (let index = 0; index < allTowns.length; index++) {
+      let town = allTowns[index];
+
+      if (town.textContent.includes(keyword)) {
+         //bold text inside li element
+         allTowns[index].style.fontWeight = 'bold';
+
+         //underline text inside li element
+         allTowns[index] = allTowns[index].style.textDecoration = 'underline';
+
+         //add town to results
+         targetTowns.push(town);
+      }   
+   }
+
+   let resultElement = document.querySelector('#result');
+   let resultsNumber = targetTowns.length;
+   resultElement.textContent += resultsNumber;
+   resultElement.textContent += ' matches found';
 }
