e4aa549e8376d08858dc22f577d19bd4c8fe2f1f
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-08-30 12:55:05 +0300

    Extract Parenthesis problem solved

--- 04. DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
+++ 04. DOM Introduction - Lab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,24 @@
 function extract(content) {
+    
+    let elementContent = document.querySelector('#' + content).textContent;
+    let elements = [];
 
+    for (let index = 0; index < elementContent.length; index++) {
+        const element = elementContent[index - 1];
+        
+        if (element === '(') {
+            let text = '';
+            for (let j = index; j < elementContent.length; j++) {
+                const symbol = elementContent[j];
+
+                if (symbol === ')') {
+                    break;
+                }
+               text += symbol;  
+            }  
+            elements.push(text); 
+        }
+    }
+
+    return elements.join('; ');
 }
