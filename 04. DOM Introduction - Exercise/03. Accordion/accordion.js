7d8393b66295ba61a8def5c5e15ff860ce7c6bd1
Vasil Dimitrov <v.b.dimitrow@gmai.com>
2021-08-30 21:46:53 +0300

    Accordion problem solved

--- 04. DOM Introduction - Exercise/03. Accordion/accordion.js
+++ 04. DOM Introduction - Exercise/03. Accordion/accordion.js
@@ -1,3 +1,13 @@
 function toggle() {
-    console.log('TODO:...');
+
+    let buttonElement = document.querySelectorAll('.button')[0];
+    let contentElement = document.querySelector('#extra');
+
+    if (contentElement.style.display === '' || contentElement.style.display === 'none') {
+        contentElement.style.display = 'block';
+        buttonElement.textContent = 'Less'
+    } else{
+        contentElement.style.display = 'none';
+        buttonElement.textContent = 'More'
+    }
 }
