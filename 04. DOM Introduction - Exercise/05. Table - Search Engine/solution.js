ed5dd6180cf1c07f90b3a8f85bb4fc878bc694ad
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-08-31 07:37:45 +0300

    Table - Search Engine problem solved

--- 04. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
+++ 04. DOM Introduction - Exercise/05. Table - Search Engine/solution.js
@@ -2,7 +2,23 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
+     let rowsElements = document.querySelector('tbody').children;
 
-   }
+     let keyword = document.querySelector('#searchField').value;
+
+     for (let index = 0; index < rowsElements.length; index++) {
+      let element = rowsElements[index];
+      element.classList.remove("select");
+     }
+    
+    for (let index = 0; index < rowsElements.length; index++) {
+       let element = rowsElements[index];
+       let elementValue = element.textContent;
+       if (elementValue.includes(keyword)) {
+          element.classList.add("select");
+       }    
+    }
+
+    document.querySelector('#searchField').value = '';
+  }
 }
