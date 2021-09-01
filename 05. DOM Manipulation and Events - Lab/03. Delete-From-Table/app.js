89388444816eab2a6fd67ae2d55399890de2ccbe
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-09-01 09:45:53 +0300

    Delete from Table problem solved

--- 05. DOM Manipulation and Events - Lab/03. Delete-From-Table/app.js
+++ 05. DOM Manipulation and Events - Lab/03. Delete-From-Table/app.js
@@ -1,3 +1,33 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let tdElements = document.getElementsByTagName('td');
+
+    let tdValues = [];
+
+    for (let index = 0; index < tdElements.length; index++) {
+        const element = tdElements[index].textContent;
+        tdValues.push(element);
+
+    }
+
+    let emailsTdElements = tdValues.filter(e => e.includes('@'));
+
+    let input = document.querySelector('input');
+    let inputValue = input.value;
+    let result = '';
+    
+    for (let index = 0; index < tdElements.length; index++) {
+        const element = tdElements[index];
+
+        if (element.textContent.includes(inputValue)) {
+            let parent = element.parentElement;
+            parent.remove();
+            result = 'Deleted.';
+            break;
+        } else {
+            result = 'Not found.';
+        }
+    }
+
+    let resultElement = document.querySelector('#result');
+    resultElement.textContent = result;
 }
