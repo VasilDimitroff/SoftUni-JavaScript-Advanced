1d228f79ee5a7985d28cb962f293cbb30462823d
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-09-01 12:14:02 +0300

    Highlight Active problem solved

--- 05. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
+++ 05. DOM Manipulation and Events - Lab/05. Highlight-Active/app.js
@@ -1,3 +1,19 @@
 function focused() {
-    console.log('TODO:...');
+    let mainDivElement = document.getElementsByTagName('div')[0];
+    let divElements = mainDivElement.children;
+
+    for (let index = 0; index < divElements.length; index++) {
+        let element = divElements[index];
+        element.addEventListener('mouseover', onMouseOver);
+       
+        element.addEventListener('mouseout', onMouseOut);     
+    }
+    
+    function onMouseOver(e){
+        this.classList.add('focused');
+    }
+
+    function onMouseOut(e){
+        this.classList.remove('focused');
+    }
 }
