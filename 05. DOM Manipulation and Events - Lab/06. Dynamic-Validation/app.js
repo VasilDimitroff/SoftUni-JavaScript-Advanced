474f3e3841b59e7b3ba9726f9b6ab4c98d9a5fc9
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-09-02 11:31:46 +0300

    Dynamic Validation problem solved

--- 05. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
+++ 05. DOM Manipulation and Events - Lab/06. Dynamic-Validation/app.js
@@ -1,3 +1,16 @@
 function validate() {
-    console.log('TODO:...');
+    let input = document.querySelector('#email');
+    input.addEventListener('change', checkValidity)
+
+    function checkValidity(e){
+        if (!input.value.includes('@') || !input.value.includes('.') || !input.value.match(/[a-z]+/)) {
+            input.classList.add('error');
+        }
+
+        else {
+            input.classList.remove('error') || null;
+           input.value = '';
+        }
+        
+    }
 }
