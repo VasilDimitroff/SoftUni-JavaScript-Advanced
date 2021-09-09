b1e5489540822f7c2c84e095c11a749f618a1255
Vasil Dimitrov <v.b.dimitrow@gmai.com>
2021-09-09 23:21:58 +0300

    Encode and Decode Messages problem

--- 05. DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ 05. DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,60 @@
-function encodeAndDecodeMessages() {
-    console.log('TODO...')
+function encodeAndDecodeMessages(){
+    
+    let buttons = document.querySelectorAll('button');
+    let sendButton = buttons[0];
+    let decodeButton = buttons[1];
+
+
+    sendButton.addEventListener('click', encodeText);
+    decodeButton.addEventListener('click', decodeText);
+ 
+   let textAreas = document.querySelectorAll('textarea');
+   let sendArea = textAreas[0];
+   let recieverArea = textAreas[1];
+
+   let encodedInput = '';
+   let decodedInput = '';
+
+   function encodeText(e){ 
+    recieverArea.value = '';
+    for (let index = 0; index < sendArea.value.length; index++) {
+        let letter = sendArea.value[index];
+        let code = sendArea.value.charCodeAt(index);
+        let newCode = code + 1;
+        letter = String.fromCharCode(newCode);
+        encodedInput += letter; 
+    }
+    
+    sendArea.value = '';
+    recieverArea.value = encodedInput;
+    decodedInput = '';
+    encodedInput = '';
+   }
+
+   function decodeText(e){
+
+    recieverArea.value = '';
+    for (let index = 0; index < sendArea.value.length; index++) {
+        let letter = sendArea.value[index];
+        let code = sendArea.value.charCodeAt(index);
+        let newCode = code + 1;
+        letter = String.fromCharCode(newCode);
+        encodedInput += letter; 
+    }
+
+    let encodedValue = encodedInput;  
+    sendArea.value = '';
+    recieverArea.value = encodedInput;
+    decodedInput = '';
+
+    for (let index = 0; index < encodedValue.length; index++) {
+        let letter = encodedValue[index];
+        let code = encodedValue.charCodeAt(index);
+        let newCode = code - 1;
+        letter = String.fromCharCode(newCode);
+        decodedInput += letter; 
+    }
+
+    recieverArea.value = decodedInput;
+  }
 }
