94a2dee8c738f01f36711db3d663ddc67caf7c08
Vasil Dimitrov <v.b.dimitrow@gmai.com>
2021-09-09 22:24:02 +0300

    Fill Dropdown problem solved

--- 05. DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
+++ 05. DOM Manipulation and Events - Exercise/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,16 @@
 function addItem() {
-    console.log('TODO:...');
+    let newItemTextElement = document.querySelector('#newItemText');
+    let newItemValueElement = document.querySelector('#newItemValue');
+
+    let selectElement =  document.querySelector('#menu');
+
+    let newOptionElement = document.createElement('option');
+
+    newOptionElement.textContent = newItemTextElement.value;
+    newOptionElement.value = newItemValueElement.value;
+
+    newItemValueElement.value = '';
+    newItemTextElement.value = '';
+   
+    selectElement.appendChild(newOptionElement);
 }
