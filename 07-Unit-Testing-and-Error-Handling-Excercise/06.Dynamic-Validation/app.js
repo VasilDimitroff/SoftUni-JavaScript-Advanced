7ebd0cd32e1cca6772341b4cc84816d3391f80de
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-10-08 18:21:48 +0300

    Dynamic Validation problem solved

--- 07-Unit-Testing-and-Error-Handling-Excercise/06.Dynamic-Validation/app.js
+++ 07-Unit-Testing-and-Error-Handling-Excercise/06.Dynamic-Validation/app.js
@@ -1,3 +1,15 @@
 function validate() {
-    // TODO
+    let validationRegex = /^[a-z]+@{1}[a-z]+\.[a-z]+$/;
+
+   let usernameInput = document.querySelector('input');
+   usernameInput.addEventListener('change', checkValidity);
+
+   function checkValidity(){
+       if(!validationRegex.test(usernameInput.value)){
+            usernameInput.classList.add('error');
+       } else {
+           usernameInput.classList.remove('error');
+       }
+   }
+
 }
