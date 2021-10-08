f7fbcc660f45231496c878d6e1e8e6cfe2f01d1c
Vasil Dimitrov <v.b.dimitrow@gmail.com>
2021-10-08 18:06:53 +0300

    Notification problem solved

--- 07-Unit-Testing-and-Error-Handling-Excercise/05.Notification/app.js
+++ 07-Unit-Testing-and-Error-Handling-Excercise/05.Notification/app.js
@@ -1,4 +1,17 @@
 function notify(message) {
-  console.log('todo')
-  // TODO:
+
+  let button = document.querySelector('button');
+
+  let messageDiv = document.querySelector('#notification'); 
+  messageDiv.textContent = message;
+
+  messageDiv.style.display = 'block';
+  messageDiv.addEventListener('click', hideMessage);
+
+  function hideMessage(e){
+    let div = e.currentTarget;
+    if(div.style.display == 'block'){
+      div.style.display = 'none';
+    }
+  } 
 }
